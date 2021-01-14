72f771b3a248b232cbc70855d61cfce99f0a816c
unknown <juan.bueno01@correo.usa.edu.co>
2021-01-14 10:48:04 -0500

    agrega producto

--- js/script.js
+++ js/script.js
@@ -15,10 +15,42 @@ $("#productos").click(function(e){
   e.preventDefault();
   $('#mod3').modal('show');
 });
+$("#agregarpedi").click(function(e){
+  e.preventDefault();
+  mostrar_productos(e)
+});
+$('.ui.dropdown')
+  .dropdown()
+;
 $('select.dropdown')
   .dropdown()
 ;
 
-$('.ui.dropdown')
-  .dropdown()
-;
+function mostrar_productos(e){
+  texto=  `<div class="three fields">
+            <div class="field can">
+              <select class="ui fluid search dropdown">
+                <option value="">Descripcion del Producto</option>
+                <option value="1">buzos</option>
+                <option value="2">chaquetas</option>
+              </select>
+            </div>
+            <div class="field can">
+              <div id="cantidad1">
+                <div class="mas-menos">
+                  <i class="minus circle icon"></i>
+                </div>
+              <div>
+                <input id="cantidad" type="text" name="Catidad" value=""  placeholder="1" readonly>
+              </div>
+              <div class="mas-menos">
+                <i class="plus circle icon"></i>
+              </div>
+            </div>
+          </div>
+          <div class="field can">
+            <input type="text" placeholder="">
+          </div>
+        </div>`
+        $("#Pedido").append(texto);
+}
