eddc2c71be92e92a0ef464495ad95e80b73eb4ae
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2021-01-16 10:55:43 -0500

    Peticiones

--- js/peticion.js
+++ js/peticion.js
@@ -1,38 +1,75 @@
+// Variables
 url = 'http://184.72.83.24:5000';
+let nombreCliente = document.getElementById('nombreCliente');
+let telCliente = document.getElementById('searchTel').value;
+var productos=[];
+var categorias=[];
+var clientes=[];
+var roles=[];
+var usuarios=[];
 
+// Funciones get
 $(function(){
     $.get(url+'/producto',function(data){
         console.log(data.productos);
+        productos = data.productos;
+        llenarProductos();
     });
 });
 
 $(function(){
     $.get(url+'/categoria',function(data){
         console.log(data.categorias);
+        categorias = data.categorias;
     });
 });
 
 $(function(){
     $.get(url+'/cliente',function(data){
         console.log(data.clientes);
-        var a = data.clientes;
-        var tels = '';
-        var i;
-        for(i=0; i<a.length; i++){
-            tels = '<option value="'+(i+1)+'">'+a[i].telefono+'</option>';
-        };
-        $('#searchTel').append(tels);
+        clientes = data.clientes;
+        llenarTelefonos();
     });
 });
 
 $(function(){
     $.get(url+'/usuario',function(data){
         console.log(data.usuarios);
+        usuarios = data.usuarios;
     });
 });
 
 $(function(){
     $.get(url+'/rol',function(data){
         console.log(data.Roles);
+        roles = data.Roles;
     });
+});
+
+// Funciones varias
+function llenarTelefonos(){
+    var tels = '';
+    var i;
+    for(i=0; i<clientes.length; i++){
+        tels = '<option value="'+clientes[i].telefono+'">'+clientes[i].telefono+'</option>';
+    };
+    $('#searchTel').append(tels);
+};
+
+function llenarProductos(){
+    var tels = '';
+    var i;
+    for(i=0; i<productos.length; i++){
+        tels = '<option value="'+(i+1)+'">'+productos[i].descripcion+'</option>';
+    };
+    $('#p').append(tels);
+};
+
+$('#searchTel').change(function(){
+    telCliente = document.getElementById('searchTel').value;
+    for(i = 0; i<clientes.length; i++){
+        if (telCliente===clientes[i].telefono){
+            nombreCliente.value = clientes[i].primerNombre+' '+clientes[i].primerApellido;
+        }
+    }
 });
