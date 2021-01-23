f30d0e898e31503c3eff18d32c5df2c335736c9d
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2021-01-22 19:40:33 -0500

    Intento de esconder item del menu

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,15 @@
-var permisos = usuario.permisos
+var rolToken;
+
 function menu(){
-    
+    while(true){
+        if (usuarioToken!=undefined){
+            rolToken = usuarioToken.rol;
+            break;
+        }
+    }
+    for(var i=0;i<rolToken.permisos.length;i++){
+        if (rolToken.permisos[i]==="Orden.ver"){
+            $("#VentasItemMenu").hide()
+        }
+    }
 }
