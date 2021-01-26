293d3bfce25c1e9236b50443be95e8afbb72dac1
Santiago43 <santipego0001@gmail.com>
2021-01-26 16:37:48 -0500

    Se retiran funciones de modal de productos en orden

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var x=0;
+
 $("#agregarorden").click(function(){
 	$('#mod').modal('show');
 });
@@ -15,11 +15,7 @@ $("#productos").click(function(e){
   e.preventDefault();
   $('#mod3').modal('show');
 });
-$("#agregarpedi").click(function(e){
-  e.preventDefault();
-  mostrar_productos(x);
-  x=x+1;
-});
+
 $('.ui.dropdown')
   .dropdown()
 ;
@@ -34,45 +30,3 @@ $("#agregarproducto").click(function(){
 });
 
 
-function mostrar_productos(e){
-  texto=  '<div class="four fields " id="v'+e+'">'+
-            '<div class="field can">'+
-              '<select class="ui fluid search dropdown">'+
-                '<option value="">Descripcion del Producto</option>'+
-                '<option value="1">buzos</option>'+
-                '<option value="2">chaquetas</option>'+
-              '</select>'+
-            '</div>'+
-            '<div class="field can">'+
-              '<div id="cantidad1">'+
-                '<div class="mas-menos">'+
-                  '<i class="minus circle icon"></i>'+
-                '</div>'+
-              '<div>'+
-                '<input id="cantidad" type="text" name="Catidad" value=""  placeholder="1" readonly>'+
-              '</div>'+
-              '<div class="mas-menos">'+
-                '<i class="plus circle icon"></i>'+
-              '</div>'+
-            '</div>'+
-          '</div>'+
-          '<div class="field can">'+
-            '<input type="text" placeholder="">'+
-          '</div>'+
-          '<div class="field can x">'+
-            '<i id="xv'+e+'" class="xv2 times circle outline icon"></i>'+
-          '</div>'+
-        '</div>'+
-        '<script>'+
-        '$(".ui.dropdown")'+
-          '.dropdown();'+
-        '</script>'
-        $("#Pedido").append(texto);
-        eliminar_productos(e);
-}
-
-function eliminar_productos(e){
-  $('#xv'+e).click(function(){
-    $('#v'+e).remove();
-  });
-}
